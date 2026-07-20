298bbabaf170e5ec0c5d4d0d04b175a75c0fc186
Jhoni Faria <jhonifarias16@gmail.com>
2026-07-20 00:30:45 -0300

    feat: separe environment files

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api/v1',
-  wsUrl: 'ws://localhost:8080/api/v1/ws',
+  apiBaseUrl: 'http://149.28.119.252:8080/api/v1',
+  wsUrl: 'ws://149.28.119.252:8080/api/v1/ws',
   apiToken: 'dev-token',
 };
